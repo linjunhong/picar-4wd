b90dadbbaf892081697ae999103e4597f678363a
Chun Lin <chunlin@microsoft.com>
2021-02-09 23:07:33 -0800

    Fix always stop

--- lab/move.py
+++ lab/move.py
@@ -19,7 +19,7 @@ def move(direction, distance):
     i = 0
     distance_travelled = 0
     fc.backward(10)
-    while distance_travelled < distance or i > 20:
+    while distance_travelled < distance or i < 20:
         time.sleep(0.1)
         distance_travelled += speed4() * 0.1
         print("%smm"%distance_travelled)
