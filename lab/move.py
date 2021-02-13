d27752a1548e7afcea0bc8b10868ae0bfe85bdce
Chun Lin <chunlin@microsoft.com>
2021-02-12 22:58:23 -0800

    Change

--- lab/move.py
+++ lab/move.py
@@ -86,7 +86,7 @@ def map_environment():
     # Reset servo
     fc.get_distance_at(0)
 
-    img = (data, data, data)
+    img = (environment, environment, environment)
     cv2.imwrite('color_img.jpg', img)
     cv2.imshow("image", img)
 
