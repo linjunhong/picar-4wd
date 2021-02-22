dcf36bbe3dcd94043a76093b98bd32f1dc10afc1
Chun Lin <chunlin@microsoft.com>
2021-02-21 16:11:54 -0800

    changes

--- lab/detect.py
+++ lab/detect.py
@@ -140,7 +140,7 @@ def detect(arg_labels, arg_interpreter, arg_threshold, preview):
         #m_image = cv2.resize(image, (0,0), fx = 0.2, fy = 0.2, interpolation=cv2.INTER_NEAREST)
 
         start_time = time.monotonic()
-        results = detect_objects(interpreter, m_image, arg_threshold)
+        results = detect_objects(interpreter, image, arg_threshold)
         elapsed_ms = (time.monotonic() - start_time) * 1000
 
         print("Elapsed time (ms): ", elapsed_ms)
