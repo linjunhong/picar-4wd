9b2193592a6671553ecd3ff2940d8a10b0802a67
Chun Lin <chunlin@microsoft.com>
2021-02-21 16:14:05 -0800

    changes

--- lab/detect.py
+++ lab/detect.py
@@ -135,12 +135,12 @@ def detect(arg_labels, arg_interpreter, arg_threshold, preview):
         stream.seek(0)
         
         image = Image.open(stream).convert('RGB').resize(
-          (int(input_width/2), int(input_height/2)), Image.ANTIALIAS)
+          (input_width, input_height), Image.ANTIALIAS)
           
-        #m_image = cv2.resize(image, (0,0), fx = 0.2, fy = 0.2, interpolation=cv2.INTER_NEAREST)
+        m_image = cv2.resize(image, (0,0), fx = 0.2, fy = 0.2, interpolation=cv2.INTER_NEAREST)
 
         start_time = time.monotonic()
-        results = detect_objects(interpreter, image, arg_threshold)
+        results = detect_objects(interpreter, m_image, arg_threshold)
         elapsed_ms = (time.monotonic() - start_time) * 1000
 
         print("Elapsed time (ms): ", elapsed_ms)
