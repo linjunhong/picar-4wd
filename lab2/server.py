c3374a297aa2f65164ba41b598fd7751dcb36480
Chun Lin <chunlin@microsoft.com>
2021-03-04 23:16:19 -0800

    Update server.py

--- lab2/server.py
+++ lab2/server.py
@@ -5,7 +5,7 @@ import sys
 import threading
 
 def listening_wifi():
-    HOST = "192.168.3.49" # IP address of your Raspberry PI
+    HOST = "192.168.1.110" # IP address of your Raspberry PI
     PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
