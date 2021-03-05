5b7cd9b0f59682847d390e04b054e1279796c2e8
Chun Lin <chunlin@microsoft.com>
2021-03-04 22:21:23 -0800

    Update server.py

--- lab2/server.py
+++ lab2/server.py
@@ -2,22 +2,15 @@ import getopt
 import socket
 import sys
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+def get_wifi_socket():
+    HOST = "192.168..1.110" # IP address of your Raspberry PI
+    PORT = 65432            # Port to listen on (non-privileged ports are > 1023)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen()
 
-    try:
-        while 1:
-            client, clientInfo = s.accept()
-            print("server recv from: ", clientInfo)
-            data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-            if data != b"":
-                print(data)     
-                client.sendall(data) # Echo back to client
-    except: 
-        print("Closing socket")
-        client.close()
-        s.close()    
+    return s
 
 def start():
     wifi_socket = get_wifi_socket()
