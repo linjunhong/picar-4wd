fe7c42245981ef1b4275ef6a807fea7110002542
Chun Lin <chunlin@microsoft.com>
2021-03-04 22:58:32 -0800

    Update server.py

--- lab2/server.py
+++ lab2/server.py
@@ -1,3 +1,4 @@
+import bluetooth
 import getopt
 import socket
 import sys
@@ -12,23 +13,47 @@ def get_wifi_socket():
 
     return s
 
+def get_bluetooth_socket():
+    hostMACAddress = "DC:A6:32:C7:72:C6" # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+    port = 0
+    backlog = 1
+    size = 1024
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    s.bind((hostMACAddress, port))
+    print("listening on port ", port)
+
+    return s
+
+
 def start():
     wifi_socket = get_wifi_socket()
+    bt_socket = get_bluetooth_socket()
+    bt_socket.listen(1)
 
     try:
         wifi_client, wifi_client_info = wifi_socket.accept()
-        print("server received from:", wifi_client_info)
+        print("[wifi] server received from:", wifi_client_info)
+
+        bt_client, bt_clientInfo = s.accept()
+        print("[bt] server received from:", bt_clientInfo)
 
         while 1:
             data = wifi_client.recv(1024)
             if (data != b""):
-                print(data)
+                print("Receive data from wifi:", data)
                 wifi_client.sendall(data)
+
+            data = bt_client.recv(1024)
+            if data:
+                print(data)
+                client.send(data)
 
     except:
         print("Closing server")
         wifi_client.close()
         wifi_socket.close()
+        bt_client.close()
+        bt_socket.close()
 
     return
 
