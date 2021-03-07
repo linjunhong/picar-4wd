831553357db117e9898dc5bb0058182109b92201
Chun Lin <chunlin@microsoft.com>
2021-03-06 22:46:50 -0800

    get data back

--- lab2/picar.py
+++ lab2/picar.py
@@ -23,7 +23,7 @@ def get_data():
         'distance': distance,
     }
 
-    return json.dumps(data)
+    return bytes(json.dumps(data), 'utf-8')
 
 def move(direction, distance):
     speed = Speed(25)
